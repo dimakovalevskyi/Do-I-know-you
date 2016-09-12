796511a7babf779ad7eaa5be061348a8592d5b5d
Dmytro Kovalevskyi <dimakovalevskyi@gmail.com>
2016-09-12 13:41:00 +0300

    Completed algorithm

--- js/app.js
+++ js/app.js
@@ -11,18 +11,18 @@ angular.module("dikyApp",[])
 
 	$scope.chain=[];//chain of friends from first to second user
 
-	$scope.chainData=[];//chain which contain user`s data
+	$scope.chainData=[];//chain which contains user`s data
 //---Behaviour for input--------------------------------------------------------------
 	$scope.userUpdate = function (index) {//action onChange for inputs
 		if($scope.users[index].id!=""){//if input is not empty
 		$http.jsonp("https://api.vk.com/method/users.get?"+//get info about user
 				"user_ids="+$scope.users[index].id+
 				"&fields=photo_200,photo_100,photo_50"+
-				"&v=5.53&callback=JSON_CALLBACK").success(function(data){
+				"&v=5.53&https=1&callback=JSON_CALLBACK").success(function(data){
 			if(data.response==undefined){//if data is correct
 				$scope.users[index].valid=false;//set input`s text as invalid
 			} else{//if(data.response==undefined)
-				if(data.response[0].first_name!="DELETED"){
+				if(data.response[0].deactivated==undefined){
 					$scope.users[index].valid=true;//set input`s text as valid
 					$scope.users[index].name=data.response[0].first_name + " " 
 										  +  data.response[0].last_name;//print user name
@@ -32,14 +32,14 @@ angular.module("dikyApp",[])
 						if(data.response[0].photo_100!=undefined){//if user has photo 100px
 							$scope.users[index].photoUrl=data.response[0].photo_100;//show user photo
 						}else{
-							$scope.users[index].photoUrl=data.response[0].photo_200;//show user photo
+							$scope.users[index].photoUrl=data.response[0].photo_50;//show user photo
 						}
 					}
 				}else{
 					$scope.users[index].valid=false;
 				}
 			}
-		}).error(function(){//if request finised with error
+		}).error(function(){//if request finished with error
 			$scope.users[index].valid=false;//set input`s text as valid
 		})}//if($scope.users[index].id!="")
 		else $scope.users[index].valid=false;//set input`s text as valid
@@ -47,44 +47,69 @@ angular.module("dikyApp",[])
 //---Behaviour for button-------------------------------------------------------------
 	$scope.letsFind=function (){
 		if($scope.users[0].id==$scope.users[1].id){
-			alert("You entered two the same users!");
+			alert("Хорошая попытка, тестировщик!");
 		}else{//if($scope.users[0].id==$scope.users[1].id)
-			if(($scope.users[0].name=="DELETED ")||
-			   ($scope.users[1].name=="DELETED ")){
-				alert("You entered DELETER user!");
-			}else{//if(users deleted?)
-				$scope.inProgress=true;//set page as being in calculating
-
-				$http.jsonp("https://api.vk.com/method/users.get?"+//get info about user
-					"user_ids="+$scope.users[0].id+
-					"&v=5.53&callback=JSON_CALLBACK").success(function(data){
-					
-					$scope.users[0].id=data.response[0].id;//be sure that id is entered
-					
-					$http.jsonp("https://api.vk.com/method/users.get?"+//get info about user
-					"user_ids="+$scope.users[1].id+
-					"&v=5.53&callback=JSON_CALLBACK").success(function(data){
-
-						$scope.users[1].id=data.response[0].id;//be sure that id is entered
-
-						$scope.showChain=false;
-						$scope.chain=[];//clear chain
-						$scope.chain.push($scope.users[1].id);//add to chain second user
-
-						alert("Во время выполнения алгоритма, возможно, все будет дико лагать. Все потому что код выполняется на стороне клиента, да, тебя. Я понимаю что такой подход не самый лучший, но мне нужно было что-то сварганить по быстрому. Так что, потерпи братан.");
-						find(parseInt($scope.users[0].id) , parseInt($scope.users[1].id));//begin find from first to second
-						
-					});//.success(function(data) user1
-				});//.success(function(data) user0
+			alert("Во время выполнения алгоритма, возможно, все будет дико лагать. Все потому что код выполняется на стороне клиента, да, тебя. Я понимаю что такой подход не самый лучший, но мне нужно было что-то сварганить по быстрому. Так что, потерпи братан.");
+			setIdsInsteadUserLinks();
 			}
-		}
+	}
+//---Preperarion before calculating---------------------------------------------------
+	function setIdsInsteadUserLinks(){
+		$http.jsonp("https://api.vk.com/method/users.get?"+//get info about user
+		"user_ids="+$scope.users[0].id+
+		"&v=5.53&https=1&callback=JSON_CALLBACK").success(function(data){
+			
+			$scope.users[0].id=data.response[0].id;//be sure that id is entered
+					
+			$http.jsonp("https://api.vk.com/method/users.get?"+//get info about user
+			"user_ids="+$scope.users[1].id+
+			"&v=5.53&https=1&callback=JSON_CALLBACK").success(function(data){
+
+				$scope.users[1].id=data.response[0].id;//be sure that id is entered
+
+				swapUsersIfNeed();
+			});//.success(function(data) user1
+		});//.success(function(data) user0
+	}
+
+	function swapUsersIfNeed(){
+
+		var countOfFriendsFirstUser;
+		var countOfFriendsSecondUser;
+
+		$http.jsonp("https://api.vk.com/method/friends.get?"+//get info about user`s friends
+		"user_id="+$scope.users[0].id+
+		"&v=5.53&callback=JSON_CALLBACK").success(function(data){
+			
+			countOfFriendsFirstUser=data.response.count;//be sure that id is entered
+					
+			$http.jsonp("https://api.vk.com/method/friends.get?"+//get info about user`s friends
+			"user_id="+$scope.users[1].id+
+			"&v=5.53&callback=JSON_CALLBACK").success(function(data){
+
+				countOfFriendsSecondUser=data.response.count;//be sure that id is entered
+
+				if(countOfFriendsFirstUser>countOfFriendsSecondUser){
+					alert("Для ускорения работы алгоритма пользователи будут поменяны местами");
+					var temp = $scope.users[0];//swapping two users
+					$scope.users[0]=$scope.users[1];//swapping two users
+					$scope.users[1]=temp;//swapping two users
+				}
+				$scope.inProgress=true;//set page as being in calculating
+				$scope.showChain=false;
+				$scope.chain=[];//clear chain
+				$scope.chain.push($scope.users[1].id);//add to chain second user
+				find(parseInt($scope.users[0].id) , parseInt($scope.users[1].id));//begin find from first to second
+							
+			});//.success(function(data) user1
+		});//.success(function(data) user0
 	}
 //---Some MAGIC-----------------------------------------------------------------------
 	function find(firstUser, secondUser){
 		var queue = [];//queue of users must be checked later
 		var checked = [];//list of checked users
 
-		var currentUser= firstUser;//start seek from first user
+		$scope.currentUser= firstUser;//start seeking from first user
 		var finded=false;
 
 		var friendsOfSecondUser;
@@ -97,7 +122,7 @@ angular.module("dikyApp",[])
 				friendsOfSecondUser=data.response.items;
 	
 				if(friendsOfSecondUser.indexOf(firstUser) != -1){//if first and second users are friends
-					$scope.chain.unshift(firstUser);//add first user to begin of chain
+					$scope.chain.unshift(firstUser);//add first user to the beginning of chain
 					updateChain();
 				}else{//if(friendsOfSecondUser.indexOf(firstUser) != -1)
 					findRecursion();//call algorithm
@@ -106,7 +131,7 @@ angular.module("dikyApp",[])
 		});
 
 		function findRecursion(){
-			$http.jsonp("https://api.vk.com/method/friends.get?"+"user_id="+currentUser+"&v=5.53&callback=JSON_CALLBACK")//get current user`s friends list 
+			$http.jsonp("https://api.vk.com/method/friends.get?"+"user_id="+$scope.currentUser+"&v=5.53&callback=JSON_CALLBACK")//get current user`s friends list 
 				.success(function(data){
 				if(data.response!=undefined){//if data is correct
 					for(var i = 0 ; i<data.response.count ; i++){
@@ -115,7 +140,7 @@ angular.module("dikyApp",[])
 
 								finded=true;//to stop recursion
 								$scope.chain.unshift(data.response.items[i]);//add to chain two last users
-								$scope.chain.unshift(currentUser);//add to chain two last users
+								$scope.chain.unshift($scope.currentUser);//add to chain two last users
 								i=data.response.count;//to stop for(...)
 	
 							}else{//if(friendsOfSecondUser.indexOf(data.response.items[i]) != -1)
@@ -125,8 +150,8 @@ angular.module("dikyApp",[])
 					}//for(var i = 0 ; i<data.response.count ; i++)
 				}//if(data.response!=undefined)
 	
-				checked.push(currentUser);//add user to checked list
-				currentUser = queue.shift();//take out next user from queue
+				checked.push($scope.currentUser);//add user to checked list
+				$scope.currentUser = queue.shift();//take out next user from queue
 	
 				if((queue.length==0)||(finded)){//if algorythm is done
 	
--- js/vkapi.js
+++
@@ -1,7 +0,0 @@
-	$http.jsonp("https://api.vk.com/method/friends.get?"+
-				"user_id=999999"+
-				"&v=5.53&callback=JSON_CALLBACK").success(function(data){
-		console.log(data);
-	}).error(function(){
-
-	});
