7fcc4625e755736da5d0311b3e14e59fe782fd61
Dmytro Kovalevskyi <dimakovalevskyi@gmail.com>
2016-09-29 15:10:05 +0300

    Fixed path error

--- js/directives.js
+++ js/directives.js
@@ -4,7 +4,7 @@ application.directive("bigProfiles",function () {
 			scope.usersData = scope[attributes["bigProfiles"]];
 		},
 		restrict: "AE",
-		templateUrl: 'bigProfiles.html'
+		templateUrl: 'views/bigProfiles.html'
 		//template: '<div class="col-xs-6" ng-repeat="user in usersData"><img class="img-thumbnail user-photo" src="{{user.photoUrl}}" ng-show="user.valid"/><p class="lead text-center" ng-show="user.valid"><a class="user-link"  href="{{"http://vk.com/id"+user.id}}" ng-bind="user.name"></a></p></div>'
 	}//return
 });//application.directive("bigProfiles",function () {
